b01acb85c13881dd1caf017054094217b5cd8a5f
Sethionic <user@nonexistant.domain>
2013-04-13 02:08:32 -0400

    Added Availability Library js

---
+++ js/Available.js
@@ -0,0 +1,45 @@
+//This file is essentially a library for <select> population calls
+//It is to be updated as features or words are added to the project
+
+var A_Chapters= [
+{text: 'Chapter 3',value: '3'},
+{text: 'Chapter 4',value: '4'},
+{text: 'Chapter 5',value: '5'},
+{text: 'Chapter 6',value: '6'},
+{text: 'Chapter 7',value: '7'},
+{text: 'Chapter 8',value: '8'},
+{text: 'Chapter 9',value: '9'},
+{text: 'Chapter 10',value: '10'},
+{text: 'Chapter 11',value: '11'},
+{text: 'Chapter 12',value: '12'},
+{text: 'Chapter 13',value: '13'},
+{text: 'Chapter 14',value: '14'},
+{text: 'Chapter 15',value: '15'},
+{text: 'Chapter 16',value: '16'},
+{text: 'Chapter 17',value: '17'},
+{text: 'Chapter 18',value: '18'}];
+
+A_POS=[
+{text: 'Adjective',value: 'A'},
+{text: 'Adverb',value: 'B'},
+{text: 'Conjunction',value: 'C'},
+{text: 'Interjection',value: 'I'},
+{text: 'Particle',value: 'L'},
+{text: 'Noun',value: 'N'},
+{text: 'Preposition',value: 'P'},
+{text: 'Pronoun',value: 'R'},
+{text: 'Definite Article',value: 'T'},
+{text: 'Verb',value: 'V'},
+{text: 'Indeclinable',value: 'Z'}];
+
+A_Case=[
+{text: 'Accusative',value: 'A'},
+{text: 'Dative',value: 'D'},
+{text: 'Genitive',value: 'G'},
+{text: 'Nominative',value: 'N'}];
+
+A_Number=[
+{text: 'Singular',value: '1'},
+{text: 'Plural',value: '2'}];
+
+
